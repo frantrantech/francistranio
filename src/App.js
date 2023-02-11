890a461608a5c413b317f08b63dbf9770c065bf7
Francis Tran <francuhz4@gmail.com>
2023-02-10 19:49:00 -0600

    Update text for initial AWS push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yak Fran Site
         </a>
       </header>
     </div>
