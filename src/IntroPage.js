75355a701718c6842ec6919e8a094f875415a313
Francis Tran <francuhz4@gmail.com>
2023-03-16 02:30:13 -0500

    Text in box properly.

--- src/IntroPage.js
+++ src/IntroPage.js
@@ -9,9 +9,9 @@ function IntroPage() {
     <div className="background-container">
       <img src="firstscreen.png" className="background-image" alt="Background" />
 
-      <div className="overlay-image"> 
-        <img src ="terminal.svg"/>
-        <div className = "introduction"> hi</div>
+      <div className="terminal-wrapper"> 
+        <img src ="terminal.svg" />
+        <div className = "introduction"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</div>
       </div>
     </div>
   );
