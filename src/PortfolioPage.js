d763a3518dce1fb66ffa7c3517ce05e6ba4e207f
Francis Tran <francuhz4@gmail.com>
2023-03-17 16:07:32 -0500

    Adjusted card image location.

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -10,18 +10,18 @@ function PortfolioPage() {
             <div className = "cardsWrapper">
                 <div className="card">
                     <a href="https://github.com/yungfran/Application-Filler-Python">
-                        <img src="resume-portfolio-svgrepo-com.svg"/>
+                        <img className="cardImg"src="resume-portfolio-svgrepo-com.svg"/>
                     </a>
                 </div>
 
                 <div className="card">
-                    <a href="https://github.com/yungfran/Application-Filler-Python">
+                    <a href="https://github.com/yungfran/interrupted_me">
                         <img src="discord-icon-svgrepo-com.svg"/>
                     </a>
                 </div>
 
                 <div className="card">
-                    <a href="https://github.com/yungfran/Application-Filler-Python">
+                    <a href="https://github.com/yungfran/gtg">
                         <img src="pulling-up-training-silhouette-svgrepo-com.svg"/>
                     </a>
                 </div>
