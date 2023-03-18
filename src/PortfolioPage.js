f5b7f0e75ed6e7c8b9eea5efc57f795d9a8dd385
Francis Tran <francuhz4@gmail.com>
2023-03-18 18:27:08 -0500

    Looks good, pushing to main

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -31,23 +31,25 @@ function PortfolioPage() {
       <div className="portfolioText">MY PORTFOLIO</div>
 
       <div className="cardsWrapper">
-        <div className="card" ref={(el) => (cardsRef.current[0] = el)}>
-          <a href="https://github.com/yungfran/Application-Filler-Python">
-            <img className="cardImg" src="resume-portfolio-svgrepo-com.svg" />
-          </a>
-        </div>
 
-        <div className="card" ref={(el) => (cardsRef.current[1] = el)} id="discord">
-          <a href="https://github.com/yungfran/interrupted_me">
-            <img src="discord-mark-black.svg" />
-          </a>
-        </div>
-
-        <div className="card" ref={(el) => (cardsRef.current[2] = el)}>
+      <div className="card" ref={(el) => (cardsRef.current[0] = el)}>
           <a href="https://github.com/yungfran/gtg">
             <img src="pulling-up-training-silhouette-svgrepo-com.svg" />
           </a>
         </div>
+
+      <div className="card" ref={(el) => (cardsRef.current[1] = el)}>
+        <a href="https://github.com/yungfran/Application-Filler-Python">
+          <img className="cardImg" src="resume-portfolio-svgrepo-com.svg" />
+        </a>
+      </div>
+
+      <div className="card" ref={(el) => (cardsRef.current[2] = el)} id="discord">
+        <a href="https://github.com/yungfran/interrupted_me">
+          <img src="discord-mark-black.svg" className="filter-green"/>
+        </a>
+      </div>
+
       </div>
     </div>
   );
