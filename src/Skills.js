3e8c723e696db0736556838f3d84c21887a0ef9b
Francis Tran <francuhz4@gmail.com>
2023-03-19 18:19:39 -0500

    Fixed skill animation to always display the full bar.

--- src/Skills.js
+++ src/Skills.js
@@ -32,30 +32,37 @@ function Skills() {
       <div className="skills-text">SKILLS</div>
       <div className="grid-container">
         <div className="line-animiation-wrapper">
+
           <div className="div1 color-animation-90 " ref={(ref) => cardsRef.current.push(ref)}>
             <span className="text-slide-animation " ref={(ref) => cardsRef.current.push(ref)}>Java</span>
-          </div>
-          <div className="div2 color-animation-80" ref={(ref) => cardsRef.current.push(ref)}>
-            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Python</span>
-          </div>
-          <div className="div3 color-animation-80" ref={(ref) => cardsRef.current.push(ref)}>
-            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>TypeScript / React / Node</span>
-          </div>
-          <div className="div4 color-animation-70" ref={(ref) => cardsRef.current.push(ref)}>
-            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Swift</span>
           </div>
           <div className="div5 color-animation-90 " ref={(ref) => cardsRef.current.push(ref)}>
             <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Backend Systems</span>
           </div>
+
+
+          <div className="div2 color-animation-80" ref={(ref) => cardsRef.current.push(ref)}>
+            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Python</span>
+          </div>
           <div className="div6 color-animation-70 " ref={(ref) => cardsRef.current.push(ref)}>
             <span className="text-slide-animation " ref={(ref) => cardsRef.current.push(ref)}>Amazon Web Services</span>
+          </div>
+
+          <div className="div3 color-animation-80" ref={(ref) => cardsRef.current.push(ref)}>
+            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>TypeScript / React / Node</span>
           </div>
           <div className="div7 color-animation-60" ref={(ref) => cardsRef.current.push(ref)}>
             <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Full Stack Web Development</span>
           </div>
+
+
+          <div className="div4 color-animation-70" ref={(ref) => cardsRef.current.push(ref)}>
+            <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>Swift</span>
+          </div>
           <div className="div8 color-animation-50" ref={(ref) => cardsRef.current.push(ref)}>
             <span className="text-slide-animation" ref={(ref) => cardsRef.current.push(ref)}>iOS</span>
           </div>
+
         </div>
       </div>
     </div>
