fa7442f1da2d1eaa7725c2ee7486bd4a92e96003
Francis Tran <francuhz4@gmail.com>
2023-03-20 17:59:43 -0500

    Contact section no links yet.

--- src/Contact.js
+++ src/Contact.js
@@ -2,9 +2,23 @@ import './Contact.css'
 
 function Contact () {
     return (
-        <div className="contactWrapper">
-            <div className='contactText'> GET IN TOUCH </div>
-            
+        <div className="contact-wrapper">
+            <div className="contact-text-wrapper">
+                <div className='get-in-touch'> GET IN TOUCH </div>
+                <div className = "draw-line"> </div>
+                
+                <div className = "contact-text-placement">
+                    <div className="contact-text">  Don't hesitate to reach out for any questions </div>
+
+                    <div className="contact-text">
+                        You can contact me at <a href="mailto:francuhz4@gmail.com" target="_top">francuhz4@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+            </footer>
+
         </div>
     )
 }
