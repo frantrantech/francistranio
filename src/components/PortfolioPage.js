48e1023405cff0ec37944ebff7c01bd7a4e51760
Francis Tran <francuhz4@gmail.com>
2023-05-29 00:55:22 -0500

    Layout in 2x2 | terminal

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -52,88 +52,93 @@ function PortfolioPage() {
   
   */
   return (
-    <div className="PortfolioWrapper">
+    <div className="portfolio-wrapper">
       <div className="portfolioText" ref={portfolioTextRef}>PORTFOLIO</div>
-
-      <div className="cardsWrapper">
-
-        {/* Need 2 a tags to make the work and animate the lines properly*/}
-        <div className="portfolio-card" ref={(el) => (cardsRef.current[0] = el)}>
-          <span>
+      
+      <div className="portfolio-flex-wrapper">
+        <div className="cardsWrapper">
+          {/* Need 2 a tags to make the work and animate the lines properly*/}
+          <div className="portfolio-card" ref={(el) => (cardsRef.current[0] = el)}>
             <span>
-              <a href="https://github.com/yungfran/gtg" target="_blank">
-                <span className="app-name">
-                    GTG Calisthenics
+              <span>
+                <a href="https://github.com/yungfran/gtg" target="_blank">
+                  <span className="app-name">
+                      GTG Calisthenics
 
-                    <div className="app-description">
-                    A web application to help you track your calisthenics progress.
-                    </div>
+                      <div className="app-description">
+                      A web application to help you track your calisthenics progress.
+                      </div>
+                  </span>
+                </a>
+              </span>
+            </span>
+            <a href="https://github.com/yungfran/gtg" target="_blank">
+              <img src="pulling-up-training-silhouette-svgrepo-com.svg" />
+            </a>
+          </div>
+
+          <div className="portfolio-card" ref={(el) => (cardsRef.current[1] = el)}>
+              <span>
+                <span>
+                  <a href="https://github.com/yungfran/Application-Filler-Python" target="_blank">
+                    <span className="app-name">
+                        Application Filler
+
+                      <div className="app-description">
+                        A CLI tool to automatically apply to jobs on Lever.
+                      </div>
+                    </span>
+                  </a>
                 </span>
+              </span>
+              <a href="https://github.com/yungfran/Application-Filler-Python" target="_blank">
+                <img className="cardImg" src="resume-portfolio-svgrepo-com.svg" />
               </a>
-            </span>
-          </span>
-          <a href="https://github.com/yungfran/gtg" target="_blank">
-            <img src="pulling-up-training-silhouette-svgrepo-com.svg" />
-          </a>
-        </div>
+          </div>
 
-        <div className="portfolio-card" ref={(el) => (cardsRef.current[1] = el)}>
-            <span>
+          <div className="portfolio-card" ref={(el) => (cardsRef.current[2] = el)}>
               <span>
-                <a href="https://github.com/yungfran/Application-Filler-Python" target="_blank">
-                  <span className="app-name">
-                      Application Filler
+                <span>
+                  <a href="https://github.com/yungfran/moodle-iOS" target="_blank">
+                    <span className="app-name">
+                        Moodle
 
-                    <div className="app-description">
-                      A CLI tool to automatically apply to jobs on Lever.
-                    </div>
-                  </span>
-                </a>
+                        <div className="app-description">
+                        iOS app for tracking mood
+                        </div>
+                    </span>
+                  </a>
+                </span>
               </span>
-            </span>
-            <a href="https://github.com/yungfran/Application-Filler-Python" target="_blank">
-              <img className="cardImg" src="resume-portfolio-svgrepo-com.svg" />
-            </a>
-        </div>
+              <a href="https://github.com/yungfran/moodle-iOS" target="_blank">
+                <img src="moodle.svg" className="filter-green"/>
+              </a>
+          </div>
 
-        <div className="portfolio-card" ref={(el) => (cardsRef.current[2] = el)}>
-            <span>
+
+          <div className="portfolio-card" ref={(el) => (cardsRef.current[3] = el)}>
               <span>
-                <a href="https://github.com/yungfran/moodle-iOS" target="_blank">
-                  <span className="app-name">
-                      Moodle
+                <span>
+                  <a href="https://github.com/yungfran/interrupted_me" target="_blank">
+                    <span className="app-name">
+                        interrupted_me
 
-                      <div className="app-description">
-                       iOS app for tracking mood
-                      </div>
-                  </span>
-                </a>
+                        <div className="app-description">
+                        An all in one discord bot to annoy all your friends by interrupting them when they speak
+                        </div>
+                    </span>
+                  </a>
+                </span>
               </span>
-            </span>
-            <a href="https://github.com/yungfran/moodle-iOS" target="_blank">
-              <img src="moodle.svg" className="filter-green"/>
-            </a>
+              <a href="https://github.com/yungfran/interrupted_me" target="_blank">
+                <img src="discord-mark-black.svg"/>
+              </a>
+          </div>
         </div>
 
-
-        <div className="portfolio-card" ref={(el) => (cardsRef.current[3] = el)}>
-            <span>
-              <span>
-                <a href="https://github.com/yungfran/interrupted_me" target="_blank">
-                  <span className="app-name">
-                      interrupted_me
-
-                      <div className="app-description">
-                      An all in one discord bot to annoy all your friends by interrupting them when they speak
-                      </div>
-                  </span>
-                </a>
-              </span>
-            </span>
-            <a href="https://github.com/yungfran/interrupted_me" target="_blank">
-              <img src="discord-mark-black.svg" className="filter-green"/>
-            </a>
-        </div>
+        <div className="portfolio-terminal">
+            <img src="square_terminal.svg"/>
+          </div>
       </div>
     </div>
   );
